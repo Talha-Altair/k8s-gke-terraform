6b8e3ac1f13fd491f009ae3ced69e944d38b6a55
Talha-Altair <cooltalha27@gmail.com>
2021-12-02 23:37:17 +0530

    Updates for 2021-12-02 23:37:17

--- providers.tf
+++ providers.tf
@@ -1,7 +1,9 @@
 provider "google" {
+
   project     = var.project_id
   region      = var.region
   credentials = file(var.creds_file_path)
+
 }
 
 data "google_client_config" "default" {}
@@ -11,5 +13,6 @@ provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+
 }
 
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Terraform Module to Provision a GKE Cluster and Deploy a Microservice
+
+## Setup
+
+- Remove provider.tf file and place everything else in a module folder and invoke it from your main.tf file
+- Configure providers for kubernetes and gcloud as per your requirement
