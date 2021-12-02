36483deac224a2ad3e8801a596d62df427a366e0
Talha-Altair <cooltalha27@gmail.com>
2021-12-02 20:17:01 +0530

    Updates for 2021-12-02 20:17:01

--- k8s-provider.tf
+++ k8s-provider.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
 
-  host     = google_container_cluster.primary.endpoint
+  host     = "https://${google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
 }
